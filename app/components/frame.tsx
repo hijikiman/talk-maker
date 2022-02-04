961f36edb0694745b411aedc8aaad9ba2ac1bf71
hijikiman <30966790+hijikiman@users.noreply.github.com>
2022-02-04 23:49:40 +0900

    [add] add Head to Frame

--- app/components/frame.tsx
+++ app/components/frame.tsx
@@ -2,7 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 const Frame = ({ children }: { children: React.ReactNode }) => {
-    return <></>
+    return (
+        <>
+            <Head>
+                <title>Talk Maker</title>
+                <meta name="description" content="Generate message app screen" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </>
+    )
 }
 
 export default Frame

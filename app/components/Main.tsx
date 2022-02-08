d3a0ff5220af1dca721ce7a5ff2b0ddea985c0b9
hijikiman <30966790+hijikiman@users.noreply.github.com>
2022-02-08 14:28:19 +0900

    [add] show your message balloon and add color argument

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Reply from '../components/Reply'
 
 const Main = () => {
+    const color = { is_receive: '#ffffff', is_not_receive: '#94df84' }
     return (
         <>
             <div className="flex justify-center items-center pt-20 pb-14">
@@ -14,7 +15,8 @@ const Main = () => {
                 <div className="w-80 px-2.5">
                     <div className="h-80 bg-blue-400">
                         <div>
-                            <Reply message={'hogehoge'} />
+                            <Reply is_receive message={'hogehoge'} color={color} />
+                            <Reply message={'hogehoge'} color={color} />
                         </div>
                     </div>
                 </div>
--- app/components/Reply.tsx
+++ app/components/Reply.tsx
@@ -1,20 +1,55 @@
-const Reply = ({ is_receive, message }: { is_receive: bool; message: string }) => {
+interface Color {
+    is_receive: string
+    is_not_receive: string
+}
+
+const Reply = ({
+    is_receive,
+    message,
+    color,
+}: {
+    is_receive: bool
+    message: string
+    color: Color
+}) => {
+    const switch_color = () => {
+        return is_receive ? color.is_receive : color.is_not_receive
+    }
+
     return (
-        <>
-            <div className="pl-3">
-                <div className="triangle-left" />
-                <span className="bg-white rounded-2xl px-4 py-3">{message}</span>
+        <div>
+            <div
+                className={'px-3 relative flex' + (is_receive ? ' justify-start' : ' justify-end')}
+            >
+                <div className={is_receive ? 'triangle-left' : 'triangle-right'} />
+                <div className="inline-block balloon-background-color rounded-2xl px-4 py-3">
+                    <span>{message}</span>
+                </div>
             </div>
             <style jsx>{`
                 .triangle-left {
-                    background: white;
-                    width: 10px;
-                    height: 10px;
+                    background: ${color.is_receive};
+                    width: 13px;
+                    height: 13px;
                     clip-path: polygon(100% 0, 0 0, 100% 100%);
-                    transform: translate(-5px, 3px);
+                    position: absolute;
+                    left: 6px;
+                    top: 4px;
+                }
+                .triangle-right {
+                    background: ${color.is_not_receive};
+                    width: 13px;
+                    height: 13px;
+                    clip-path: polygon(100% 0, 0 0, 0 100%);
+                    position: absolute;
+                    right: 6px;
+                    top: 4px;
+                }
+                .balloon-background-color {
+                    background-color: ${switch_color()};
                 }
             `}</style>
-        </>
+        </div>
     )
 }
 

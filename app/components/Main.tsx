5820ac154f40f94aa282ec9845a2ede2e50d0546
hijikiman <30966790+hijikiman@users.noreply.github.com>
2022-02-08 14:39:47 +0900

    [add] manage message content in state

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,8 +1,21 @@
 import Image from 'next/image'
 import Reply from '../components/Reply'
+import { useState } from 'react'
 
 const Main = () => {
+    const [messageList, setMessageList] = useState([
+        {
+            is_receive: false,
+            message: 'hogehoge',
+        },
+        {
+            is_receive: true,
+            message: 'moja',
+        },
+    ])
+
     const color = { is_receive: '#ffffff', is_not_receive: '#94df84' }
+
     return (
         <>
             <div className="flex justify-center items-center pt-20 pb-14">
@@ -14,10 +27,19 @@ const Main = () => {
                 </div>
                 <div className="w-80 px-2.5">
                     <div className="h-80 bg-blue-400">
-                        <div>
-                            <Reply is_receive message={'hogehoge'} color={color} />
-                            <Reply message={'hogehoge'} color={color} />
-                        </div>
+                        <ul>
+                            {messageList.map((item, key) => (
+                                <li key={key}>
+                                    <div>
+                                        <Reply
+                                            is_receive={item.is_receive}
+                                            message={item.message}
+                                            color={color}
+                                        />
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>

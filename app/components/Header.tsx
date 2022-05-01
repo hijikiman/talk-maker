30afe21a29d5dd34f0c0ec4681a67e04c5795013
hijikiman <30966790+hijikiman@users.noreply.github.com>
2022-05-01 18:04:55 +0900

    [update] when mobile size, centerize header logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Header = () => {
     return (
         <header className="h-16 border-solid border-b border-gray-300">
-            <div className="max-w-5xl mx-auto my-0 py-1 px-3">
+            <div className="max-w-5xl mx-auto my-0 py-1 px-3 flex justify-center sm:justify-start">
                 <Link href="/">
                     <a>
                         <Image src="/logo.png" alt="Vercel Logo" width={334} height={56} />
